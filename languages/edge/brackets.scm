28ee776fcfabe9a2275414a663fd50e21c3d3dd4
Abdelmadjid Cherfaoui <abdelmadjid.cherfaoui@gmail.com>
2025-05-21 10:20:02 -0400

    fix highlighting in Edge Helpers

--- languages/edge/brackets.scm
+++ languages/edge/brackets.scm
@@ -1,25 +1,5 @@
-; Edge Template Bracket Matching
-
-; HTML tag pairs
+; EdgeJS minimal bracket matching
 (open_tag) @open
 (close_tag) @close
-
-; Edge directive pairs
 (if_directive) @open
 "@end" @close
-
-(each_directive) @open
-"@end" @close
-
-(component_directive) @open
-"@end" @close
-
-(slot_directive) @open
-"@end" @close
-
-; Output expression delimiters
-"{{" @open
-"}}" @close
-
-; Standard brackets
-["(" ")"] @bracket.paren
--- languages/edge/highlights.scm
+++ languages/edge/highlights.scm
@@ -1,33 +1,39 @@
-; Edge Template Syntax Highlighting
+; EdgeJS Syntax Highlighting - Comment Focus
 
-; HTML Tags
+; HTML elements
 (tag_name) @tag
-
-; Attributes
 (attribute_name) @tag.attribute
 (attribute_value) @string
 (quoted_attribute_value) @string
 
-; Edge Directives
-(if_directive "@if") @keyword.control
-(else_directive "@else") @keyword.control
-(else_directive "@elseif") @keyword.control
-(each_directive "@each") @keyword.control
-(component_directive "@component") @keyword.control
-(slot_directive "@slot") @keyword.control
-(include_directive "@include") @keyword.function
-(let_directive "@let") @keyword
-(raw_directive "@" @operator (identifier) @function)
+; Edge directives
+(if_directive) @keyword.control
+(else_directive) @keyword.control
+(each_directive) @keyword.control
+(component_directive) @keyword
+(slot_directive) @keyword
+(include_directive) @keyword
+(let_directive) @keyword
+
+; Raw directive components
+"@" @keyword.directive
+(directive_expression) @function.directive
+
+; Each directive special syntax
+(each_params "in") @keyword
 
 ; Output expressions
-(output_expression "{{" @punctuation.bracket
-                    "}}" @punctuation.bracket)
+"{{" @punctuation.special
+"}}" @punctuation.special
+"{{{" @punctuation.special
+"}}}" @punctuation.special
 
-; JavaScript expressions
-(js_expression) @expression
-
-; Comments
-(comment) @comment
-
-; Identifiers
+; Expression components
 (identifier) @variable
+(string) @string
+(number) @number
+(method_call) @function.call
+(member_expression) @property
+
+; Comments with different styling
+(comment) @comment.dim
